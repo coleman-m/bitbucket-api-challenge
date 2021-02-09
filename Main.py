0bdd66396e0b3921b75024503690a093389946f8
coleman-m <GPa$$P0tato>
2021-02-09 15:52:30 -0800

    Working first draft: Fetch commits, sort, and print

--- Main.py
+++ Main.py
@@ -1,1 +1,29 @@
-# This is the beginning
+import urllib.request as rq
+import json
+import datetime
+
+def parse_datetime(dt: str) -> datetime.datetime:
+    return datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S%z')
+
+def sort_datetime(val: tuple) -> datetime.datetime:
+    return parse_datetime(val[0])
+
+def do_request(url: str, method: str) -> json:
+    request = rq.Request(url, method=method)
+    response = rq.urlopen(request)
+    response_data = json.loads(response.read())
+    response.close()
+    return response_data
+
+def extract_timestamp_message_array(obj: [json]) -> [(datetime.datetime, str)]:
+    result = list()
+    for index in range(len(obj)):
+        result.append((obj[index]['date'], obj[index]['message']))
+    return result
+
+if __name__ == '__main__':
+    response_obj = do_request('https://bitbucket.org/api/2.0/repositories/calanceus/interviews17/commits', 'GET')
+    arr = extract_timestamp_message_array(response_obj['values'])
+    arr.sort(key=sort_datetime, reverse=True)
+    for i in range(len(arr)):
+        print(arr[i])
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # bitbucket-api-challenge
-Coleman McClelland's submission for Calance's coding challenge (Feb. 2021)
+Coleman McClelland's submission for Calance's coding challenge _Feb. 2021_
+
+---
+### Part 1:
+
+We often interface with web APIs of applications to read and write data. We would like you to write a small program or script that accesses the BitBucket API.  Your code should pull the commit messages from all of the check-ins for the repository linked below, then write those messages and their timestamps to a text file in reverse chronological order.  
+
+Repository location: https://bitbucket.org/calanceus/interviews17 
+
+The solution can be in any language you choose.
+
+### Part 2: 
+
+For the second part, we would like you to send your code into that same repository.
