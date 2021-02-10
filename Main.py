6b2701bd8f16f4c0e86aa033e21231cf220ff481
coleman-m <GPa$$P0tato>
2021-02-09 16:13:15 -0800

    Added ability to write output to a file

--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@ import datetime
 def parse_datetime(dt: str) -> datetime.datetime:
     return datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S%z')
 
+
 def sort_datetime(val: tuple) -> datetime.datetime:
     return parse_datetime(val[0])
+
 
 def do_request(url: str, method: str) -> json:
     request = rq.Request(url, method=method)
@@ -15,15 +17,25 @@ def do_request(url: str, method: str) -> json:
     response.close()
     return response_data
 
+
 def extract_timestamp_message_array(obj: [json]) -> [(datetime.datetime, str)]:
     result = list()
     for index in range(len(obj)):
         result.append((obj[index]['date'], obj[index]['message']))
     return result
 
+
+def write_tuples_to_file(file: str, array: [tuple]) -> None:
+    file = open(file, 'w')
+    for i in range(len(array)):
+        file.write(" ".join(array[i]))
+        file.write("\n")
+    file.close()
+    return
+
+
 if __name__ == '__main__':
     response_obj = do_request('https://bitbucket.org/api/2.0/repositories/calanceus/interviews17/commits', 'GET')
-    arr = extract_timestamp_message_array(response_obj['values'])
-    arr.sort(key=sort_datetime, reverse=True)
-    for i in range(len(arr)):
-        print(arr[i])
+    commit_array = extract_timestamp_message_array(response_obj['values'])
+    commit_array.sort(key=sort_datetime, reverse=True)
+    write_tuples_to_file('coleman_bitbucket_output.txt', commit_array)
